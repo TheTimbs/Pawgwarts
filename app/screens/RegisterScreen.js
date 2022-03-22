f7470e816f59748f29f298bccb78867009640889
Chris Chun <ichrischun@gmail.com>
2022-03-22 12:57:26 -0400

    fixed register screen to upload dog array in users database

--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Button } from 'react-native';
+import { StyleSheet, Button, Image, View, Text } from 'react-native';
 import * as Yup from 'yup';
 import { createUserWithEmailAndPassword } from '@firebase/auth';
 import { db, storage, auth } from '../../firebase/firebase-config';
@@ -10,6 +10,9 @@ import { TextInput } from 'react-native-gesture-handler';
 import { collection, addDoc, setDoc, doc } from 'firebase/firestore';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
+import * as ImagePicker from 'expo-image-picker';
+import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import uuid from 'uuid';
 // const validationSchema = Yup.object().shape({
 //   name: Yup.string().required().label('Name'),
 //   email: Yup.string().required().email().label('Email'),
@@ -20,6 +23,12 @@ function RegisterScreen() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [name, setName] = useState('');
+  const [house, setHouse] = useState('');
+  const [dogName, setDogName] = useState('');
+  const [breed, setBreed] = useState('');
+  const [DOB, setDOB] = useState('');
+  const [image, setImage] = useState(null);
+  const [taskCompleted, setTaskCompleted] = useState(0);
 
   const RegisterUser = async () => {
     const user = await createUserWithEmailAndPassword(auth, email, password);
@@ -27,12 +36,54 @@ function RegisterScreen() {
       name: name,
       email: email,
       likes: 0,
+      house: house,
+      dog: [dogName, breed, DOB, image, taskCompleted],
     });
 
     setEmail('');
     setName('');
     setPassword('');
+    setDogName('');
+    setBreed('');
+    setDOB('');
+    setImage('');
   };
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+  async function uploadImage() {
+    const blob = await new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function () {
+        resolve(xhr.response);
+      };
+      xhr.onerror = function (e) {
+        console.log(e);
+        reject(new TypeError('Network request failed'));
+      };
+      xhr.responseType = 'blob';
+      xhr.open('GET', image, true);
+      xhr.send(null);
+    });
+    const fileRef = ref(getStorage(), uuid.v4());
+    const result = await uploadBytes(fileRef, blob);
+    blob.close();
+    const downloadURL = await getDownloadURL(fileRef);
+    setImage(downloadURL);
+    return downloadURL;
+  }
+
   return (
     <Screen style={styles.container}>
       <TextInput
@@ -65,6 +116,46 @@ function RegisterScreen() {
         textContentType="password"
         onChangeText={(text) => setPassword(text)}
       />
+      <TextInput
+        style={styles.TextInput}
+        autoCapitalize="none"
+        autoCorrect={false}
+        icon="account"
+        name="dogName"
+        placeholder="Dog Name"
+        onChangeText={(text) => setDogName(text)}
+      />
+      <TextInput
+        style={styles.TextInput}
+        autoCapitalize="none"
+        autoCorrect={false}
+        icon="account"
+        name="breed"
+        placeholder="Dog Breed"
+        onChangeText={(text) => setBreed(text)}
+      />
+      <TextInput
+        style={styles.TextInput}
+        autoCapitalize="none"
+        autoCorrect={false}
+        icon="account"
+        name="DOB"
+        placeholder="Date of Birth (mm-dd-yyyy)"
+        onChangeText={(text) => setDOB(text)}
+      />
+      <Button title="Select Profile Picture" onPress={pickImage} />
+      {image ? (
+        <Image
+          source={{ uri: image }}
+          style={{
+            width: 150,
+            height: 150,
+            borderRadius: 150 / 2,
+            alignSelf: 'center',
+          }}
+        />
+      ) : null}
+      <Button title="Confirm Profile Picture" onPress={uploadImage} />
       <Button title="Register" onPress={() => RegisterUser()} />
     </Screen>
   );
