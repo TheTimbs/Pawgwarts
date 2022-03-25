b1c87fa5e913bd922cc857a9158e613ee4b19b54
ErickManz <erickmanzanares22@gmail.com>
2022-03-25 13:18:48 -0400

    refactor the home screen

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -5,83 +5,80 @@ function Home(props){
 
   return (
 
-       <ImageBackground
-      blurRadius={1}
-      style={styles.background}
-      source={require('../assets/BlueBackground.jpeg')}
-    >
-      <Text style={styles.textHeader}>Team Current points:</Text>
-      <View style={styles.container}>
+       <ImageBackground blurRadius={1}  style={styles.background}
+      source={require('../assets/BlueBackground.jpeg')} >
+       <View style={styles.container}>
+         <View style={styles.top}>
+           <Text>Hello</Text>
+         </View>
+          <View style={styles.center}>
+            <Text style={styles.text}>Team Current points</Text>
+          </View>
+           <View style={styles.bottom}>
+              <View style={styles.box}>
+                <ImageBackground  style= {styles.inner} source={require('../assets/hufflepup.png')}>
+                </ImageBackground>
+                <Text style={styles.text}>team 1</Text>
+              </View>
+              <View style={styles.box}>
+                <ImageBackground  style= {styles.inner} source={require('../assets/ravenpaw.jpeg')}>
+                </ImageBackground>
+                <Text style={styles.text}>team 1</Text>
+              </View>
+              <View style={styles.box}>
+               <ImageBackground  style= {styles.inner} source={require('../assets/slobberin.jpeg')}>
+               </ImageBackground>
+               <Text style={styles.text}>team 1</Text>
+              </View>
+              <View style={styles.box}>
+               <ImageBackground  style= {styles.inner} source={require('../assets/gryffindog.jpeg')}>
+               </ImageBackground>
+               <Text style={styles.text}>team 1</Text>
+              </View>
+            </View>
 
-        <View style={styles.box}>
-          <View style={styles.inner}>
-            <Image  style= {styles.image} source={require('../assets/Puppy.jpeg')}></Image>
-            <Text style={styles.text}>team 1</Text>
-          </View>
-        </View>
-        <View style={styles.box}>
-          <View style={styles.inner}>
-            <Image  style= {styles.image} source={require('../assets/Puppy.jpeg')}></Image>
-            <Text style={styles.text}>team 2</Text>
-          </View>
-        </View>
-        <View style={styles.box}>
-          <View style={styles.inner}>
-            <Image  style= {styles.image} source={require('../assets/Puppy.jpeg')}></Image>
-            <Text style={styles.text}>team 3</Text>
-          </View>
-        </View>
-        <View style={styles.box}>
-          <View style={styles.inner}>
-            <Image  style= {styles.image} source={require('../assets/Puppy.jpeg')}></Image>
-            <Text style={styles.text}>team 4</Text>
-          </View>
-        </View>
-
-
-     </View>
+       </View>
      </ImageBackground>
 
   );
 }
 const styles = StyleSheet.create({
   container:{
-    width: '100%',
-    height: '65%',
+    flex:1
+  },
+  top:{
+    height:'45%',
+    alignItems:'center',
+    justifyContent:'center',
+
+  },
+  bottom:{
+    height: '50%',
     flexDirection: 'row',
     flexWrap: 'wrap',
-    top:'120%',
-    margin:'-8%',
-    marginLeft:"5%"
+    padding:5,
+    backgroundColor:'black'
+  },
+  center:{
+    height:'5%',
+    left:'25%'
   },
   box:{
     width: '50%',
     height: '50%',
-    margin:'-18%',
-    marginVertical:'-12%',
-    marginLeft:"1.5%"
   },
   inner:{
     flex:1,
-    alignItems:'center',
-    justifyContent:'center',
-    padding:5
-  },
-  image: {
-    width: '60%',
-    height: '60%',
-
   },
   text:{
-  bottom:'60%',
-  fontSize:40,
+  fontSize:20,
   color:"white"
   },
   background: {
    height:'100%'
   },
   textHeader:{
-    top:'50%',
+    top:'55%',
     fontSize:40,
     color:"red",
     left:10
