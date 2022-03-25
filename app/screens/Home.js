5f3eb62c7d2023e3f8e416110cc6fcd03e5e2eb3
ErickManz <erickmanzanares22@gmail.com>
2022-03-25 13:47:36 -0400

    now can see the teams points and pictures'

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,7 +1,31 @@
-
 import { ImageBackground, StyleSheet, View, Image, Text, } from 'react-native';
 import React from 'react';
+import { db, auth } from '../../firebase/firebase-config';
+import { getDoc, collection, doc, } from 'firebase/firestore';
+import { useEffect, useState } from 'react/cjs/react.development';
+
 function Home(props){
+  const gRef = doc(db,'houses ',"GryffinDog");
+  const hRef = doc(db,'houses ',"HufflePup");
+  const rRef = doc(db,'houses ',"RavenPaw");
+  const sRef = doc(db,'houses ',"Sloberin");
+  const [gPoints, setGPoints] = useState(0);
+  const [hPoints, setHPoints] = useState(0);
+  const [rPoints, setRPoints] = useState(0);
+  const [sPoints, setSPoints] = useState(0);
+  useEffect(()=>{
+    const getPoints = async() =>{
+      const dataG = await getDoc(gRef)
+      const dataH = await getDoc(hRef)
+      const dataR = await getDoc(rRef)
+      const dataS = await getDoc(sRef)
+      setGPoints(dataG.data().points)
+      setHPoints(dataH.data().points)
+      setRPoints(dataR.data().points)
+      setSPoints(dataS.data().points)
+    }
+    getPoints();
+  },[])
 
   return (
 
@@ -16,24 +40,20 @@ function Home(props){
           </View>
            <View style={styles.bottom}>
               <View style={styles.box}>
-                <ImageBackground  style= {styles.inner} source={require('../assets/hufflepup.png')}>
-                </ImageBackground>
-                <Text style={styles.text}>team 1</Text>
+                <ImageBackground  style= {styles.inner} source={require('../assets/hufflepup.png')}/>
+                <Text style={styles.text}>Points: {hPoints}</Text>
               </View>
               <View style={styles.box}>
-                <ImageBackground  style= {styles.inner} source={require('../assets/ravenpaw.jpeg')}>
-                </ImageBackground>
-                <Text style={styles.text}>team 1</Text>
+                <ImageBackground  style= {styles.inner} source={require('../assets/ravenpaw.jpeg')}/>
+                <Text style={styles.text}>Points: {rPoints}</Text>
               </View>
               <View style={styles.box}>
-               <ImageBackground  style= {styles.inner} source={require('../assets/slobberin.jpeg')}>
-               </ImageBackground>
-               <Text style={styles.text}>team 1</Text>
+                <ImageBackground  style= {styles.inner} source={require('../assets/slobberin.jpeg')}/>
+                <Text style={styles.text}>Points: {sPoints}</Text>
               </View>
               <View style={styles.box}>
-               <ImageBackground  style= {styles.inner} source={require('../assets/gryffindog.jpeg')}>
-               </ImageBackground>
-               <Text style={styles.text}>team 1</Text>
+                <ImageBackground  style= {styles.inner} source={require('../assets/gryffindog.jpeg')}/>
+                <Text style={styles.text}>Points: {gPoints}</Text>
               </View>
             </View>
 
@@ -41,6 +61,7 @@ function Home(props){
      </ImageBackground>
 
   );
+
 }
 const styles = StyleSheet.create({
   container:{
@@ -53,11 +74,10 @@ const styles = StyleSheet.create({
 
   },
   bottom:{
-    height: '50%',
+    height: '45%',
     flexDirection: 'row',
     flexWrap: 'wrap',
-    padding:5,
-    backgroundColor:'black'
+
   },
   center:{
     height:'5%',
@@ -69,6 +89,9 @@ const styles = StyleSheet.create({
   },
   inner:{
     flex:1,
+    width:"90%",
+    height:"90%",
+    left:"5%"
   },
   text:{
   fontSize:20,
