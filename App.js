002acf834993ec3f34da17ea5bf9b3d628221bff
Kazi Ahmed <kaaziahmed@gmail.com>
2022-03-27 17:49:48 -0400

    edited Home page to show trainings in progress

--- App.js
+++ App.js
@@ -4,31 +4,6 @@ import AuthNavigator from './app/navigation/AuthNavigator';
 
 export default function App() {
   return (
-<<<<<<< HEAD
-    //<WelcomeScreen />
-    // <LoginScreen />
-    // <RegisterScreen />
-    //<AccountScreen />
-    // <ListingsScreen />
-    //<MessagesScreen />
-    //<ViewImageScreen />
-   // <FeedScreen />
-    //<UploadImageScreen />
-    // <QuizScreen />
-    // <QuizResult />
-    //<SignOut />
-    // <UserProfile />
-    //<TrainingYearsScreen />
-
-    // <NavigationContainer theme={navigationTheme}>
-    //   <AuthNavigator />
-    // </NavigationContainer>
-    // <TreatStore />
-    // <ToyStore />
-    // <AccessoriesStore />
-    //     <Home></Home>
-=======
->>>>>>> trainingScreens-Refactored
     <NavigationContainer>
       <AuthNavigator />
     </NavigationContainer>
