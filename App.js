255c20d9e9ee45267e8cf7cff7b9c509ce99dad4
yaazi1 <87243578+yaazi1@users.noreply.github.com>
2022-03-28 14:46:06 -0400

    Update App.js

--- App.js
+++ App.js
@@ -4,27 +4,6 @@ import AuthNavigator from './app/navigation/AuthNavigator';
 
 export default function App() {
   return (
-    //<WelcomeScreen />
-    // <LoginScreen />
-    // <RegisterScreen />
-    //<AccountScreen />
-    // <ListingsScreen />
-    //<MessagesScreen />
-    //<ViewImageScreen />
-    //<FeedScreen />
-    //<UploadImageScreen />
-    // <QuizScreen />
-    // <QuizResult />
-    //<SignOut />
-    // <UserProfile />
-    //<TrainingYearsScreen />
-    // <TreatStore />
-    // <ToyStore />
-    // <AccessoriesStore />
-    //     <Home></Home>
-    // <NavigationContainer>
-    //   <AppNavigator />
-    // </NavigationContainer>
     <NavigationContainer>
       <AuthNavigator />
     </NavigationContainer>
